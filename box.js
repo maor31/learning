3672e696c7be2f7656188f59ab780af5b2679db1
maor31 <71394079+maor31@users.noreply.github.com>
2020-09-16 18:05:11 +0300

    Add files via upload

---
+++ box.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+function init()
+{
+
+}
+
+function clickChangeBoxes()
+{
+       changeBox()
+
+}
+
+function changeBox()
+{
+
+  box1.style.backgroundColor = "red";
+ 
+}
