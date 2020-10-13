e341f7150de4b8e2785e2905a374b6ca38c34827
maor31 <71394079+maor31@users.noreply.github.com>
2020-10-13 22:16:43 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Game from './components/Game'
+
+
+function App() {
+  return (
+    <div>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
